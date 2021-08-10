a681b8aaf14fc3be334f8c1fe7e8380af82205f3
WeikangWeng <weikangweng@pop-os.localdomain>
2021-08-10 20:02:42 +0800

    fixing directory

--- Comparison.h
+++ Comparison.h
@@ -24,7 +24,7 @@ thread_local double total_time;
 thread_local char *n;
 int looping;
 char filePath[] = "file.txt";
-char sizePath[] = "rand7.txt";
+char sizePath[] = "randinput/rand6.txt";
 FILE *pFile;
 int fSize, testBuff;
 char *buffer;
